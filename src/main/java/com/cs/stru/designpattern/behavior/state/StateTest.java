0a4c8121b993c38d62d4fc0adbefbd0338604a9e
benjaminchan0607 <benjaminchan0607@sina.com>
2018-07-26 22:47:48 +0800

    if else pattern

---
+++ src/main/java/com/cs/stru/designpattern/behavior/state/StateTest.java
@@ -0,0 +1,24 @@
+package com.cs.stru.designpattern.behavior.state;
+
+public class StateTest {
+
+    /**
+     * 定义：(源于Design Pattern)：当一个对象的内在状态改变时允许改变其行为，这个对象看起来像是改变了其类。
+     *
+     *
+     * 采用状态模式，运用多态消除if else
+     * @param args
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Hero hero = new Hero();
+        hero.startRun();
+        hero.setState(Hero.SPEED_UP);
+        Thread.sleep(1);
+        hero.setState(Hero.SPEED_DOWN);
+        Thread.sleep(1);
+        hero.setState(Hero.SWIM);
+        Thread.sleep(1);
+        hero.stopRun();
+
+    }
+}
