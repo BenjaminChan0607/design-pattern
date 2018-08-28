9d20f9c2823b86668515dd749fd8eeb5567dc5f0
chend <Mf@20171018>
2018-08-28 19:09:33 +0800

    update

--- src/main/java/com/cs/stru/designpattern/rpc/client/RpcClientTest.java
+++ src/main/java/com/cs/stru/designpattern/rpc/client/RpcClientTest.java
@@ -12,6 +12,12 @@ import java.net.InetSocketAddress;
  * RPC的实现方法：Java实现RPC
  * <p>
  * RPC传输的数据以二进制序列的形式在多个进程之间传输。发送方将Java对象转换为字节序列，才能在网络上传送；接收方将字节序列转换成Java对象
+ * <p>
+ * RPC通信方式采用的Socket是基于BIO实现的，IO效率不高，还有Java原生的序列化机制占内存太多，运行效率也不高。可以考虑从下面几种方法改进。
+ * 可以采用基于JSON数据传输的RPC框架；
+ * 可以使用NIO或直接使用Netty替代BIO实现；
+ * 使用开源的序列化机制，如Hadoop Avro与Google protobuf等；
+ * 服务注册可以使用Zookeeper进行管理，能够让应用更加稳定。
  */
 public class RpcClientTest {
 
